1a55e7f91120d05f9bdddf223a33d633c72f4456
pk00095 <pk00095@gmail.com>
2020-07-24 22:01:54 +0530

    prediction utilities

--- predict.py
+++ predict.py
@@ -1,8 +1,87 @@
 import tensorflow as tf
 from tensorflow import keras
+from PIL import Image, ImageDraw, ImageFont
+
 from helpers import YoloConfig
 from yolo3.model import yolo_eval, yolo_body, tiny_yolo_body
 
+font = ImageFont.load_default()
+
+STANDARD_COLORS = [
+    'AliceBlue', 'Chartreuse', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque',
+    'BlanchedAlmond', 'BlueViolet', 'BurlyWood', 'CadetBlue', 'AntiqueWhite',
+    'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan',
+    'DarkCyan', 'DarkGoldenRod', 'DarkGrey', 'DarkKhaki', 'DarkOrange',
+    'DarkOrchid', 'DarkSalmon', 'DarkSeaGreen', 'DarkTurquoise', 'DarkViolet',
+    'DeepPink', 'DeepSkyBlue', 'DodgerBlue', 'FireBrick', 'FloralWhite',
+    'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod',
+    'Salmon', 'Tan', 'HoneyDew', 'HotPink', 'IndianRed', 'Ivory', 'Khaki',
+    'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue',
+    'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey',
+    'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue',
+    'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime',
+    'LimeGreen', 'Linen', 'Magenta', 'MediumAquaMarine', 'MediumOrchid',
+    'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen',
+    'MediumTurquoise', 'MediumVioletRed', 'MintCream', 'MistyRose', 'Moccasin',
+    'NavajoWhite', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed',
+    'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed',
+    'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple',
+    'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Green', 'SandyBrown',
+    'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue',
+    'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue', 'GreenYellow',
+    'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White',
+    'WhiteSmoke', 'Yellow', 'YellowGreen'
+]
+
+def letterbox_image(image, size):
+    '''resize image with unchanged aspect ratio using padding'''
+    iw, ih = image.size
+    w, h = size
+    scale = min(w/iw, h/ih)
+    nw = int(iw*scale)
+    nh = int(ih*scale)
+
+    image = image.resize((nw,nh), Image.BICUBIC)
+    new_image = Image.new('RGB', size, (128,128,128))
+    new_image.paste(image, ((w-nw)//2, (h-nh)//2))
+    return new_image
+
+def annotate_image(image_name, bboxes, scores, labels, threshold=0.5, label_dict=None):
+  image = Image.open(image_name)
+  Imagedraw = ImageDraw.Draw(image)
+
+  for box, label, score in zip(bboxes, labels, scores):
+    if score < threshold:
+      continue
+
+    (left,top,right,bottom) = box
+
+    label_to_display = label
+    if isinstance(label_dict, dict):
+      label_to_display = label_dict[label]
+
+    caption = "{}|{:.3f}".format(label_to_display, score)
+    #draw_caption(draw, b, caption)
+
+    colortofill = STANDARD_COLORS[label]
+    Imagedraw.rectangle([left,top,right,bottom], fill=None, outline=colortofill)
+
+    display_str_heights = font.getsize(caption)[1]
+    # Each display_str has a top and bottom margin of 0.05x.
+    total_display_str_height = (1 + 2 * 0.05) * display_str_heights
+
+    if top > total_display_str_height:
+        text_bottom = top
+    else:
+        text_bottom = bottom + total_display_str_height
+
+    text_width, text_height = font.getsize(caption)
+    margin = np.ceil(0.05 * text_height)
+    Imagedraw.rectangle([(left, text_bottom-text_height-2*margin), (left+text_width,text_bottom)], fill=colortofill)
+
+    Imagedraw.text((left+margin, text_bottom-text_height-margin),caption,fill='black',font=font)
+
+  return image
 
 def freeze_model(model_path, config, num_classes, max_boxes=20, score_threshold=.6,iou_threshold=.5):
     # model_path = os.path.expanduser(self.model_path)
@@ -26,4 +105,25 @@ def freeze_model(model_path, config, num_classes, max_boxes=20, score_threshold=
 
     prediction_model = keras.models.Model(yolo_model.input, [boxes, scores, classes])
 
+    return prediction_model
+
+def detect_image(model, image, config):
+    start = timer()
+
+    # if self.model_image_size != (None, None):
+    #     assert self.model_image_size[0]%32 == 0, 'Multiples of 32 required'
+        # assert self.model_image_size[1]%32 == 0, 'Multiples of 32 required'
+    boxed_image = letterbox_image(image, (config.width, config.height))
+    # else:
+    #     new_image_size = (image.width - (image.width % 32),
+    #                       image.height - (image.height % 32))
+    #     boxed_image = letterbox_image(image, new_image_size)
+    image_data = np.array(boxed_image, dtype='float32')
+
+    print(image_data.shape)
+    image_data /= 255.
+    image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+
+    boxes, scores, classes = model.predict(image_data)
+    
     return boxes, scores, classes
